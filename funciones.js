483047ab71b3f2a9726efbb16b3785937beb796e
kknnn <agu_bb1995@hotmail.com>
2018-09-17 19:57:18 -0300

    agregados

--- funciones.js
+++ funciones.js
@@ -30,4 +30,18 @@ const nombresUsuarios=usuarios.map(function(unUsuario){
           return unUsuario.nombre;
           });
 
+//Es igual a:
+const nombresUsuarios=(usuarios)=>{
+	usuarios.map((unUsuario)=>unUsuario.age);
+}
+
 console.log(nombresUsuarios); //Salida: ["agustin", "antonella"]
+
+//Numeros por defecto
+function multiplicador(x,y){
+	x=x || 5;
+	y=y || 10;
+	return x*y;
+}
+
+console.log(multiplicador(2,3)); //Si la funcion multiplciador recibe parametros entonces usa esos, si no recibe usa los numeros por defecto, 5 y 10.
